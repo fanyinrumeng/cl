46d7ff90ad124ae38ca6983634c48077124663c0
fanyinrumeng <42991257+fanyinrumeng@users.noreply.github.com>
2022-08-06 00:50:14 +0800

    Update install.sh

--- content/install.sh
+++ content/install.sh
@@ -2,7 +2,7 @@
 
 # Install Cloudreve
 VERSION="$(curl --retry 10 --retry-max-time 60 https://api.github.com/repos/cloudreve/Cloudreve/releases/latest | jq .tag_name | sed 's/\"//g')"
-wget -qO - https://github.com/cloudreve/Cloudreve/releases/download/3.4.0/cloudreve_${VERSION}_linux_amd64.tar.gz | tar -zxf - -C /usr/bin
+wget -qO - https://github.com/cloudreve/Cloudreve/releases/download/${VERSION}/cloudreve_${VERSION}_linux_amd64.tar.gz | tar -zxf - -C /usr/bin
 
 # Install Aria2
 wget -qO - https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz | tar -zxf - -C /usr/bin
